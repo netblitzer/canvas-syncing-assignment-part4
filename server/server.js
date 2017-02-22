88b6ed49a7f4d8b6ea5f8757a2a9ea85a11d08d5
Luke Miller <Luke Miller>
2017-02-21 21:37:29 -0500

    Fixed things

--- server/server.js
+++ server/server.js
@@ -18,21 +18,14 @@ console.log(`Listening on localhost:${port}`);
 
 const io = socketio(app);
 
-const draws = { };
-
 io.on('connection', (socket) => {
   socket.join('room1');
 
-  socket.emit('join', { id: socket.id, draws });
-
   socket.on('sendUpdate', (data) => {
     socket.broadcast.to('room1').emit('receiveUpdate', data);
-    draws[data.id] = data;
   });
 
   socket.on('disconnect', () => {
     socket.leave('room1');
-    delete draws[socket.id];
-    socket.broadcast.to('room1').emit('deleteID', { id: socket.id });
   });
 });
